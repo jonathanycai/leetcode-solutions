6a88305aeb89c39204102af09b966dc3a1ebddf1
Jonathan Cai <137264568+jonathanycai@users.noreply.github.com>
2026-07-09 02:46:08 -0700

    Add: find-leaves-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/find-leaves-of-binary-tree/submission-0.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+
+        def dfs(node):
+            if not node:
+                return -1
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+            curr = max(left, right) + 1
+
+            if len(res) == curr:
+                res.append([])
+            
+            res[curr].append(node.val)
+            return curr
+        
+        dfs(root)
+        return res
+            
